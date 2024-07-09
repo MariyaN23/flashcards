ad722946cc5b48a293559ef70b6aa363ab6d3dde
MariyaN23 <sellaite505@gmail.com>
2024-07-09 20:39:19 +0300

    Add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-import {App} from '@/App';
+// eslint-disable-next-line import/no-unresolved
+import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
